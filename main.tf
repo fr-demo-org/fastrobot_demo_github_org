6da997c33fbe45a6c6651ca068e9313cb039fe45
lamont <lamont@fastrobot.com>
2020-11-04 11:51:07 -0800

    Updated README with todo items, terrform fmt, filter branch so that apply only happens on main

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ resource "github_repository" "this_repository" {
   name        = "fastrobot_demo_github_org"
   description = "CircleCI driven Terraform managing this GitHub Organization"
 
-  visibility   = "public"
-  has_issues   = true
-  has_projects = true
-  has_wiki     = false
+  visibility    = "public"
+  has_issues    = true
+  has_projects  = true
+  has_wiki      = false
   has_downloads = false
 
   default_branch = "main"
